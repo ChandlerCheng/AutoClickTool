7490219ac9354d1da6c486f524a02d15203638ff
chandler_chang <msdeme104@gmail.com>
2024-04-22 10:59:40 +0800

    修改為自動在DEBUG模式下 , 切換熱鍵為F1.F2 (F10.F11為Debug模式下的單步執行按鍵)

--- buttonClick/Form1.cs
+++ buttonClick/Form1.cs
@@ -45,8 +45,13 @@ namespace buttonClick
         private int actionX = 0;
         private int actionY = 0;
         private int actionDelay = 1000;
+#if DEBUG
+        private int registerHK_MainRoop = (int)Keys.F1;
+        private int registerHK_GetCoordinate = (int)Keys.F2;
+#else 
         private int registerHK_MainRoop = (int)Keys.F11;
-        private int registerHK_GetCoordinate = (int)Keys.F10;        
+        private int registerHK_GetCoordinate = (int)Keys.F10;
+#endif
         private long totalTimeoutSecondsTarget = 0;
         private long totalTimeoutSecondsNow = 0;
         Stopwatch stopwatch = new Stopwatch();
